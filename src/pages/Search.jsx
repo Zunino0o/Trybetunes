64abcc299ff505a0ee42bd52d96909f4cf4e6a2f
Lucas Zunino <zunino10@hotmail.com>
2023-01-17 00:02:28 -0300

    requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,12 +1,51 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import Input from '../components/Input';
 
 class Search extends Component {
+  state = {
+    searchInput: '',
+    isSearchButtonDisabled: true,
+  };
+
+  validateSearchButton = () => {
+    const { searchInput } = this.state;
+    const checkInputLength = (searchInput.length > 1);
+    this.setState({
+      isSearchButtonDisabled: !checkInputLength,
+    });
+  };
+
+  onInputChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value }, this.validateSearchButton);
+  };
+
   render() {
+    const { searchInput, isSearchButtonDisabled } = this.state;
+
     return (
       <>
         <Header />
-        <div data-testid="page-search">Search</div>
+        <div data-testid="page-search">
+          <form>
+            <Input
+              type="text"
+              test="search-artist-input"
+              title="Banda/Artista"
+              id="searchInput"
+              onChange={ this.onInputChange }
+              value={ searchInput }
+            />
+            <button
+              type="submit"
+              data-testid="search-artist-button"
+              disabled={ isSearchButtonDisabled }
+            >
+              Pesquisar
+            </button>
+          </form>
+        </div>
       </>
     );
   }
