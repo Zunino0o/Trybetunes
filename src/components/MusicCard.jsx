6f38316ca4cd9147d4b6b669a85e66be4d3ce19c
Lucas Zunino <zunino10@hotmail.com>
2023-01-30 18:20:24 -0300

    requisito 8

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
 
 class MusicCard extends Component {
+  state = {
+    loading: false,
+    checked: false,
+  };
+
+  favFunc = ({ target }) => {
+    this.setState({
+      loading: true,
+      checked: target.checked,
+    }, async () => {
+      await addSong(JSON.parse(target.name));
+      // .then(() => setTimeout(() => {}, '3000'))
+      this.setState({
+        loading: false,
+      });
+    });
+  };
+
   render() {
     const { info } = this.props;
-    const { previewUrl, trackName } = info;
+    const { previewUrl, trackName, trackId } = info;
+    const { loading, checked } = this.state;
     return (
       <div>
-        <h1>{ trackName }</h1>
+        <h1>{ loading ? 'Carregando...' : trackName }</h1>
         <audio data-testid="audio-component" src={ previewUrl } controls>
           <track kind="captions" />
           O seu navegador não suporta o elemento
@@ -16,6 +36,17 @@ class MusicCard extends Component {
           <code>audio</code>
           .
         </audio>
+        <label htmlFor="checkbox">
+          Favorita
+          <input
+            name={ JSON.stringify(info) }
+            data-testid={ `checkbox-music-${trackId}` }
+            type="checkbox"
+            checked={ checked }
+            onChange={ this.favFunc }
+            id="checkbox"
+          />
+        </label>
       </div>
     );
   }
